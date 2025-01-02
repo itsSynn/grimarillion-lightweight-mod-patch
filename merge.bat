98b667e5af068a333731a881fbcf55efa41cf65a
itsSynn <21230437+itsSynn@users.noreply.github.com>
2025-01-01 19:50:47 -0500

    Update merge.bat

--- merge.bat
+++ merge.bat
@@ -8,11 +8,11 @@ SET "lightweightSourceDir=%lightweightDir%\source\lightweight"
 SET "grimarillionDir=%scriptDir%\mods\grimarillion"
 SET "savesDir=%UserProfile%\Documents\My Games\Grim Dawn\save"
 
-IF NOT EXIST "%lightweightSourceDir%" (
-	ECHO ERROR - Lightweight mod source cannot be found at:
+IF NOT EXIST "%lightweightDir%" (
+	ECHO ERROR - Lightweight mod cannot be found at:
 	ECHO %scriptDir%\mods\__lightweight & ECHO.
 	ECHO Make sure the merge.bat script is at the root of your Grim Dawn installation and you've copied in the Lightweight mod source. & ECHO.
-	ECHO Press Enter to exit...
+	ECHO Press ^(Enter^) to exit...
 	PAUSE >nul
 	EXIT
 )
@@ -20,10 +20,12 @@ IF NOT EXIST "%grimarillionDir%" (
 	ECHO ERROR - Grimarillion mod cannot be found at:
 	ECHO %scriptDir%\mods\grimarillion & ECHO.
 	ECHO Make sure the merge.bat script is at the root of your Grim Dawn installation and you've installed Grimarillion. & ECHO.
-	ECHO Press Enter to exit...
+	ECHO Press ^(Enter^) to exit...
 	PAUSE >nul
 	EXIT
 )
+
+GOTO :TEMPLATES
 
 :: -- Templates
 
@@ -31,40 +33,28 @@ IF NOT EXIST "%grimarillionDir%" (
 CLS
 
 IF EXIST "%scriptDir%\database\templates" (
-	ECHO Grim Dawn templates.arc extract already exists at:
+	ECHO ^[1/6^]
+	ECHO The Grim Dawn templates.arc extract already exists at:
 	ECHO %scriptDir%\database\templates & ECHO.
-	ECHO Do you want to remove this directory and re-extract? & ECHO.
-	ECHO Type 'y' to re-extract.
-	ECHO Type 'n' to exit. & ECHO.
-) ELSE (
-	ECHO Grim Dawn templates.arc must be extracted to:
-	ECHO %scriptDir%\database\templates & ECHO.
-	ECHO Do you want to extract? & ECHO.
-	ECHO Type 'y' to extract.
-	ECHO Type 'n' to exit. & ECHO.
-)
-
-SET /p templatesInput=^>
-
-IF /I "%templatesInput%"=="y" (
-	IF EXIST "%scriptDir%\database\templates" (
-		RMDIR /s /q "%scriptDir%\database\templates"
-		ECHO. & ECHO Directory removed: %scriptDir%\database\templates
-	)
-	ECHO.
-	"%scriptDir%\ArchiveTool.exe" "%scriptDir%\database\templates.arc" -extract "%scriptDir%\database"
-	ECHO. & ECHO Press Enter to continue...
+	ECHO Press ^(Enter^) to remove this directory and re-extract...
 	PAUSE >nul
-	GOTO :DATABASE
-)
-IF /I "%templatesInput%"=="n" (
-	EXIT
+
+	RMDIR /s /q "%scriptDir%\database\templates"
+	ECHO. & ECHO Directory removed: %scriptDir%\database\templates
+) ELSE (
+	ECHO ^[1/6^]
+	ECHO The Grim Dawn templates.arc will be extracted to:
+	ECHO %scriptDir%\database\templates & ECHO.
+	ECHO Press ^(Enter^) to extract...
+	PAUSE >nul
 )
 
-ECHO. & ECHO Invalid option. & ECHO.
-ECHO Press Enter to continue...
+ECHO.
+"%scriptDir%\ArchiveTool.exe" "%scriptDir%\database\templates.arc" -extract "%scriptDir%\database"
+ECHO. & ECHO Press ^(Enter^) to continue...
 PAUSE >nul
-GOTO :TEMPLATES
+
+GOTO :DATABASE
 
 :: -- Database
 
@@ -72,29 +62,28 @@ GOTO :TEMPLATES
 CLS
 
 IF EXIST "%grimarillionDir%\database\records" (
-	ECHO Grimarillion merge records already exists at:
+	ECHO ^[2/6^]
+	ECHO The Lightweight mod database records already exists at:
 	ECHO %grimarillionDir%\database\records & ECHO.
-	ECHO Do you want to remove this directory and continue? & ECHO.
-	ECHO Type 'y' to remove the existing records.
-	ECHO Type 'n' to exit. & ECHO.
-	SET /p recordsInput=^>
-
-	IF /I "!recordsInput!"=="y" (
-		RMDIR /s /q "%grimarillionDir%\database\records"
-		ECHO. & ECHO Directory removed: %grimarillionDir%\database\records & ECHO.
-		ECHO Press Enter to continue...
-		PAUSE >nul
-		GOTO :BACKUP
-	)
-	IF /I "!recordsInput!"=="n" (
-		EXIT
-	)
-
-	ECHO. & ECHO Invalid option. & ECHO.
-	ECHO Press Enter to continue...
+	ECHO Press ^(Enter^) to remove this directory and re-copy records...
 	PAUSE >nul
-	GOTO :DATABASE
+
+	RMDIR /s /q "%grimarillionDir%\database\records"
+	ECHO. & ECHO Directory removed: %grimarillionDir%\database\records
+) ELSE (
+	ECHO ^[2/6^]
+	ECHO The Lightweight mod database records will be copied to:
+	ECHO %grimarillionDir%\database\records & ECHO.
+	ECHO Press ^(Enter^) to copy records...
+	PAUSE >nul
 )
+
+ECHO.
+XCOPY /e /y /q /i "%lightweightSourceDir%\database\records" "%grimarillionDir%\database\records"
+ECHO. & ECHO Press ^(Enter^) to continue...
+PAUSE >nul
+
+GOTO :BACKUP
 
 :: -- Backup
 
@@ -107,112 +96,96 @@ SET "time=%time:~0,8%"
 SET "backupTime=%time::=%"
 SET "backupLocation=%savesDir%\_backup_%backupDate%_%backupTime%"
 
-ECHO A backup of your characters can be created at:
+ECHO ^[3/6^]
+ECHO A backup of your characters will be created at:
 ECHO %backupLocation% & ECHO.
-ECHO Do you want to create a backup? & ECHO.
-ECHO Type 'y' to create a backup.
-ECHO Type 'n' to skip. & ECHO.
-SET /p backupInput=^>
-
-IF /I "%backupInput%"=="y" (
-	ECHO.
-	MKDIR "%backupLocation%"
-	XCOPY /e /y /q "%savesDir%\user" "%backupLocation%"
-	ECHO. & ECHO Press Enter to continue...
-	PAUSE >nul
-	GOTO :MERGE
-)
-IF /I "%backupInput%"=="n" (
-	GOTO :MERGE
-)
-
-ECHO. & ECHO Invalid option. & ECHO.
-ECHO Press Enter to continue...
+ECHO Press ^(Enter^) to create a backup...
 PAUSE >nul
-GOTO :BACKUP
+
+ECHO.
+MKDIR "%backupLocation%"
+XCOPY /e /y /q "%savesDir%\user" "%backupLocation%"
+ECHO. & ECHO Press ^(Enter^) to continue...
+PAUSE >nul
+
+GOTO :MERGE
 
 :: -- Merge
 
 :MERGE
 CLS
 
-ECHO Ready to merge.
-ECHO You must manually patch the Grimarillion database using the AssetManager after merging. & ECHO.
-ECHO Do you want to perform the merge? & ECHO.
-ECHO Type 'y' to merge.
-ECHO Type 'n' to exit. & ECHO.
-SET /p mergeInput=^>
-
-IF /I "%mergeInput%"=="y" (
-	ECHO.
-	XCOPY /e /y /q /i "%lightweightSourceDir%\database\records" "%grimarillionDir%\database\records"
-	"%scriptDir%\ArchiveTool.exe" "%grimarillionDir%\resources\Conversations.arc" -update . "%lightweightSourceDir%\resources\conversations"
-	"%scriptDir%\ArchiveTool.exe" "%grimarillionDir%\resources\Creatures.arc" -update . "%lightweightSourceDir%\resources\creatures"
-	"%scriptDir%\ArchiveTool.exe" "%grimarillionDir%\resources\Scripts.arc" -update . "%lightweightSourceDir%\resources\scripts"
-	"%scriptDir%\ArchiveTool.exe" "%grimarillionDir%\resources\Sound.arc" -update . "%lightweightSourceDir%\resources\sound"
-	"%scriptDir%\ArchiveTool.exe" "%grimarillionDir%\resources\Text_EN.arc" -update . "%lightweightSourceDir%\resources\text_en"
-	ECHO. && ECHO Merge complete. & ECHO.
-	ECHO Upon continuing, the AssetManager will automatically open so you can perform manual database patching.
-	ECHO Keep this script open and close the AssetManager once patching is complete to perform final cleanup. & ECHO.
-	ECHO Press Enter to continue...
-	PAUSE >nul
-	ECHO %scriptDir%
-	START /wait "" "%scriptDir%\AssetManager.exe"
-	GOTO :CLEANUP
-)
-IF /I "%mergeInput%"=="n" (
-	EXIT
-)
-
-ECHO. & ECHO Invalid option. & ECHO.
-ECHO Press Enter to continue...
+ECHO ^[4/6^]
+ECHO The Grimarillion resources will be modified with Lightweight mod additions. & ECHO.
+ECHO Press ^(Enter^) to modify the resources...
 PAUSE >nul
-GOTO :MERGE
+
+ECHO.
+"%scriptDir%\ArchiveTool.exe" "%grimarillionDir%\resources\Conversations.arc" -update . "%lightweightSourceDir%\resources\conversations"
+"%scriptDir%\ArchiveTool.exe" "%grimarillionDir%\resources\Creatures.arc" -update . "%lightweightSourceDir%\resources\creatures"
+"%scriptDir%\ArchiveTool.exe" "%grimarillionDir%\resources\Scripts.arc" -update . "%lightweightSourceDir%\resources\scripts"
+"%scriptDir%\ArchiveTool.exe" "%grimarillionDir%\resources\Sound.arc" -update . "%lightweightSourceDir%\resources\sound"
+"%scriptDir%\ArchiveTool.exe" "%grimarillionDir%\resources\Text_EN.arc" -update . "%lightweightSourceDir%\resources\text_en"
+ECHO. & ECHO Press ^(Enter^) to continue...
+PAUSE >nul
+
+GOTO :PATCH
+
+:: -- Manual patch
+
+:PATCH
+CLS
+
+ECHO ^[5/6^]
+ECHO The Grimarillion database must be manually patched. & ECHO.
+ECHO Press ^(Enter^) to open the AssetManager...
+PAUSE >nul
+
+ECHO.
+ECHO Waiting for the AssetManager to close...
+
+START /wait "" "%scriptDir%\AssetManager.exe"
+
+ECHO.
+ECHO The AssetManager was closed and database patching is assumed to be complete.
+ECHO. & ECHO Press ^(Enter^) to continue...
+PAUSE >nul
+
+GOTO :CLEANUP
 
 :: -- Cleanup
 
 :CLEANUP
 CLS
 
-ECHO AssetManager was closed and database patching is assumed to be complete. & ECHO.
-ECHO Do you want to perform final cleanup? & ECHO.
-ECHO Type 'y' to perform cleanup.
-ECHO Type 'n' to exit. & ECHO.
-SET /p cleanupInput=^>
-
-ECHO.
-IF /I "%cleanupInput%"=="y" (
-	SET "removedDir=false"
-	IF EXIST "%scriptDir%\database\templates" (
-		RMDIR /s /q "%scriptDir%\database\templates"
-		ECHO Directory removed: %scriptDir%\database\templates
-		SET "removedDir=true"
-	)
-	IF EXIST "%lightweightDir%" (
-		RMDIR /s /q "%lightweightDir%"
-		ECHO Directory removed: %lightweightDir%
-		SET "removedDir=true"
-	)
-	IF EXIST "%grimarillionDir%\database\records" (
-		RMDIR /s /q "%grimarillionDir%\database\records"
-		ECHO Directory removed: %grimarillionDir%\database\records
-		SET "removedDir=true"
-	)
-	IF /I "!removedDir!"=="true" (
-		ECHO.
-	)
-
-	ECHO Cleanup complete. & ECHO.
-	ECHO You can remove this .bat file after exiting. & ECHO.
-	ECHO Press Enter to exit...
-	PAUSE >nul
-	EXIT
-)
-IF /I "%cleanupInput%"=="n" (
-	EXIT
-)
-
-ECHO. & ECHO Invalid option. & ECHO.
-ECHO Press Enter to continue...
+ECHO ^[6/6^]
+ECHO Files used during patching will be cleaned up. & ECHO.
+ECHO Press ^(Enter^) to perform the cleanup...
 PAUSE >nul
-GOTO :CLEANUP
+
+SET "removedDir=false"
+IF EXIST "%scriptDir%\database\templates" (
+	RMDIR /s /q "%scriptDir%\database\templates"
+	ECHO Directory removed: %scriptDir%\database\templates
+	SET "removedDir=true"
+)
+IF EXIST "%lightweightDir%" (
+	RMDIR /s /q "%lightweightDir%"
+	ECHO Directory removed: %lightweightDir%
+	SET "removedDir=true"
+)
+IF EXIST "%grimarillionDir%\database\records" (
+	RMDIR /s /q "%grimarillionDir%\database\records"
+	ECHO Directory removed: %grimarillionDir%\database\records
+	SET "removedDir=true"
+)
+
+IF /I "%removedDir%"=="true" (
+	ECHO.
+)
+ECHO Cleanup complete.
+ECHO You can remove this .bat file after exiting.
+ECHO. & ECHO Press ^(Enter^) to exit...
+PAUSE >nul
+
+EXIT
